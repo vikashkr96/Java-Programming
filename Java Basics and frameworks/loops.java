30acad261816356abb5a4f8e3b99e78d996cff97
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-08-05 23:40:01 +0530

    Add files via upload

---
+++ Java Basics and frameworks/loops.java
@@ -0,0 +1,16 @@
+public class loops {
+    public static void main(String[] args) {
+
+        // factorial
+        int num = 5;
+        int fact = 1;
+        for(int i = 1;i<=num;i++){
+            fact*=i;
+        }
+        System.out.println(fact);
+        
+        
+
+    }
+
+}
